b2f6960a072ec9db3e045caa21efee9d1cf431b5
Iraka-C <iraka@qq.com>
2021-03-03 01:27:46 +0900

    Create the most structures

---
+++ scripts/config.js
@@ -0,0 +1,20 @@
+const mainPageItems=[{
+	name: "Introduction",
+	hint: "About me",
+	url: "./intro.html"
+},{
+	name: "Graphics",
+	hint: "Technological blogs",
+	url: "./graphics.html"
+},{
+	name: "Gadgets",
+	hint: "Interesting little things",
+	url: "./gadget.html"
+},{
+	name: "Photography",
+	hint: "What can you get out of a camera?",
+	url: "./photo.html"
+},{
+	name: "Contacts",
+	hint: "How to hear from me"
+}];
